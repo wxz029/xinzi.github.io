ee32461bba9062cc90c188fcaa080ee25584f7e2
wxz029 <127166034+wxz029@users.noreply.github.com>
2026-09-01 23:20:33 +0100

    Add Dayleaf offline service worker

---
+++ dayleaf/sw.js
@@ -0,0 +1,5 @@
+const CACHE='dayleaf-forest-v3-20260901';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./payload/01.txt','./payload/02.txt','./payload/03.txt','./payload/04.txt','./payload/05.txt','./payload/06.txt','./payload/07.txt','./payload/08.txt'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('dayleaf-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
